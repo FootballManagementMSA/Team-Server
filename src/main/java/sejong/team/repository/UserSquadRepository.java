3e86ca57bec71163e4b7a62f0cc670cbe46ec640
bes99 <pas1007@naver.com>
2024-02-05 12:10:31 +0900

    feat: squadId를 통한 스쿼드 내 선수들 조회 로직 추가 (#5)

--- src/main/java/sejong/team/repository/UserSquadRepository.java
+++ src/main/java/sejong/team/repository/UserSquadRepository.java
@@ -1,7 +1,14 @@
 package sejong.team.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sejong.team.domain.UserSquad;
 
+import java.util.List;
+
+
 public interface UserSquadRepository extends JpaRepository<UserSquad,Long> {
+    @Query("SELECT us FROM user_squad_tb us WHERE us.squadId = :squadId")
+    List<UserSquad> findBySquadId(@Param("squadId") Long squadId);
 }
