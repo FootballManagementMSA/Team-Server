d50d9f372d308adf5c4e164ba45659c11eccd648
bes99 <pas1007@naver.com>
2024-02-05 12:08:47 +0900

    feat: user 정보 반환 목적 feginclient 메소드 추가 (#5)

--- src/main/java/sejong/team/common/client/UserServiceClient.java
+++ src/main/java/sejong/team/common/client/UserServiceClient.java
@@ -5,9 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import sejong.team.common.client.dto.SizeUserTeamResponse;
+import sejong.team.common.client.dto.UserSquadResponse;
 
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
     @GetMapping("/api/user-service/users/teams/{teamId}")
     ResponseEntity<SizeUserTeamResponse> countUsersInTeam(@PathVariable(value = "teamId") Long teamId);
+
+    @GetMapping("/api/user-service/users/{userId}/squad")
+    ResponseEntity<UserSquadResponse> getUserInfoInSquad(@PathVariable(name = "userId") Long userId);
 }
