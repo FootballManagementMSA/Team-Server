73830182d88928f87aa1bda36d66de4996f2addf
bes99 <pas1007@naver.com>
2024-02-05 12:09:59 +0900

    feat: scheduleId와 teamId를 통한 squad 조회 jpql 작성 (#5)

--- src/main/java/sejong/team/repository/SquadRepository.java
+++ src/main/java/sejong/team/repository/SquadRepository.java
@@ -1,7 +1,14 @@
 package sejong.team.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sejong.team.domain.Squad;
 
+import java.util.Optional;
+
 public interface SquadRepository extends JpaRepository<Squad,Long> {
+    @Query("SELECT s FROM squad_tb s WHERE s.scheduleId = :scheduleId AND s.teamId = :teamId")
+    Optional<Squad> findSquadByScheduleIdAndTeamId(@Param("scheduleId") Long scheduleId,
+                                                   @Param("teamId") Long teamId);
 }
