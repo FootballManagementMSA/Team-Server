bdd698603cbd6a9cfd4782b23fa208486885cb03
bes99 <pas1007@naver.com>
2024-02-05 12:11:18 +0900

    feat: 스쿼드 조회 로직 개발 (#5)

--- src/main/java/sejong/team/controller/SquadController.java
+++ src/main/java/sejong/team/controller/SquadController.java
@@ -1,11 +1,24 @@
 package sejong.team.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sejong.team.global.DataResponse;
+import sejong.team.service.SquadService;
+import sejong.team.service.res.UserInSquadResponseDto;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/team-service")
 public class SquadController {
+    private final SquadService squadService;
+    @GetMapping("/schedules/{scheduleId}/teams/{teamId}/squad")
+    public DataResponse<List<UserInSquadResponseDto>> getTeamSquad(@PathVariable(name = "scheduleId")Long scheduleId,
+                                                                   @PathVariable(name = "teamId")Long teamId){
+        return new DataResponse(squadService.getTeamSquad(scheduleId,teamId));
+    }
 }
--- src/main/java/sejong/team/service/SquadService.java
+++ src/main/java/sejong/team/service/SquadService.java
@@ -2,10 +2,50 @@ package sejong.team.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sejong.team.common.client.UserServiceClient;
+import sejong.team.common.client.dto.UserSquadResponse;
+import sejong.team.domain.Squad;
+import sejong.team.domain.UserSquad;
+import sejong.team.global.MessageUtils;
+import sejong.team.global.SquadNotFoundException;
 import sejong.team.repository.SquadRepository;
+import sejong.team.repository.UserSquadRepository;
+import sejong.team.service.res.UserInSquadResponseDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SquadService {
     private final SquadRepository squadRepository;
+    private final UserSquadRepository userSquadRepository;
+    private final UserServiceClient userServiceClient;
+
+    /**
+     * @param scheduleId
+     * @param teamId
+     * @Return
+     * 1. Feign 클라이언트를 사용하여 외부 서비스로부터 UserSquadResponse를 가져온다
+     * 2. UserSquadResponse로부터 name과 image를 가져와 DTO를 생성 후 반환
+     */
+    public List<UserInSquadResponseDto> getTeamSquad(Long scheduleId, Long teamId){
+        Squad squad = squadRepository.findSquadByScheduleIdAndTeamId(scheduleId, teamId)
+                .orElseThrow(() -> new SquadNotFoundException(MessageUtils.NOT_EXIST_SQUAD));
+
+        List<UserSquad> userSquadList = userSquadRepository.findBySquadId(squad.getId());
+
+        return userSquadList.stream().map(userSquad -> {
+            UserSquadResponse userSquadResponse = userServiceClient.getUserInfoInSquad(userSquad.getUserId())
+                    .getBody();
+
+            return UserInSquadResponseDto.builder()
+                    .name(userSquadResponse.getName())
+                    .image(userSquadResponse.getImage())
+                    .xCoordinate(userSquad.getXCoordinate())
+                    .yCoordinate(userSquad.getYCoordinate())
+                    .build();
+        }).collect(Collectors.toList());
+
+    }
 }
